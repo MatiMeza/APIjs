af469a5879e26a6b6f057cb92f33d72e9f42b892
MatiMeza <matiasmeza77@gmail.com>
2023-05-01 01:02:28 -0300

    Project bases

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!')
+})
+app.post('/team/pokemons', () => {
+    res.status(200).send('Hello World!')
+})
+
+app.get('/team', () => {
+    res.status(200).send('Hello World!')
+})
+
+app.delete('/team/pokemons/:pokeid', () => {
+    res.status(200).send('Hello World!')
+})
+
+app.put('/team', () => {
+    res.status(200).send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
